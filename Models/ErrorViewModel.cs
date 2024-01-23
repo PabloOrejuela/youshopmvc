3dfd50fbb5792ee8d281665f0c137835c301f9bd
PabloOrejuela <porejuelac@gmail.com>
2024-01-23 16:11:02 -0500

    Versión inicial del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace YouShop.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,8 @@
+namespace YouShop.Models {
+
+    public class Usuario {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+    }
+}
